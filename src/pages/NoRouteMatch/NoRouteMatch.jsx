960cc5acca99dd87b241d9d1b94c10ae3a8007cd
Namrata-J <namratajain29001@gmail.com>
2022-06-06 23:36:47 +0530

    feat: added ui to 404 page

--- src/pages/NoRouteMatch/NoRouteMatch.jsx
+++ src/pages/NoRouteMatch/NoRouteMatch.jsx
@@ -2,8 +2,9 @@ import "./noRouteMatch.css";
 
 const NoRouteMatchPage = () => {
     return (
-        <div className="ms_noRouteMatch-page ms_flex-c">
-            <p></p>
+        <div className="ms_noRouteMatch-page ms_flex-c ms_fw-b">
+            <img src="https://i.pinimg.com/originals/29/07/f3/2907f3653381fda5feb72620a8ed1eb8.png" />
+            <p>Ooops!! Looks like this page doesn't exist</p>
         </div>
     );
 }
