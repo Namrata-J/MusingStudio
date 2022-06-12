b73fa642b4984eb90fe1dc625e4f4c14b39476be
Namrata-J <namratajain29001@gmail.com>
2022-06-12 19:52:11 +0530

    added common class to private pages

--- src/pages/watchLater/WatchLater.js
+++ src/pages/watchLater/WatchLater.js
@@ -1,5 +1,5 @@
 import "./watchLater.css";
-import { Header, SideBar } from "../../components";
+import { Header, SideBar, WatchLaterVideosListing } from "../../components";
 
 const WatchLaterPage = () => {
     return (
@@ -9,6 +9,10 @@ const WatchLaterPage = () => {
             </div>
             <div className="ms_watchlaterpage-main-section ms_flex">
                 <SideBar />
+                <div className="ms_privateVideoPage-content-section content-section ms_flex">
+                    <h4 className="ms_fw-b">Liked Videos</h4>
+                    <WatchLaterVideosListing />
+                </div>
             </div>
         </div>
     );
